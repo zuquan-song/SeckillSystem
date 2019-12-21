811436d598200e0a5eb5808296f2f33c2aba2f79
zuquan-song <zs1243@nyu.edu>
2019-12-21 17:48:40 -0500

    update

---
+++ src/main/java/com/seckill/controller/SampleController.java
@@ -0,0 +1,16 @@
+package com.seckill.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/demo")
+public class SampleController {
+
+    @RequestMapping("/thymeleaf")
+    public String thymeleaf(Model model) {
+        model.addAttribute("name", "Joshua");
+        return "hello";
+    }
+}
